eb8faf6f31aefeb5374b4ac24ddd96bac6fce945
Andy Nguyen <iam.andynguwin@gmail.com>
2026-03-05 04:28:33 -0800

    Updated images

--- init/2_init_data.sql
+++ init/2_init_data.sql
@@ -16,9 +16,9 @@ INSERT INTO idol (idol_id, idol_name, artist_id, card_set_id, current_print, ima
 (3, 'Danielle Marsh', 1, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812377/danielle_hwxery.png'),
 (4, 'Kang Haerin', 1, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812377/haerin_uwubml.png'),
 (5, 'Lee Hyein', 1, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png'),
-(6, 'Kim Chaewon', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png'),
-(7, 'Miyawaki Sakura', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png'),
-(8, 'Huh Yunjin', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png'),
-(9, 'Nakamura Kazuha', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png'),
-(10, 'Hong Eunchae', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1768812378/hyein_mhpr1m.png')
+(6, 'Kim Chaewon', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1772713067/chaewon_ourkwv.png'),
+(7, 'Miyawaki Sakura', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1772713554/sakura_mpybht.png'),
+(8, 'Huh Yunjin', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1772713067/yunjin_pbyrhg.png'),
+(9, 'Nakamura Kazuha', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1772713568/kazuha_x2zjlt.png'),
+(10, 'Hong Eunchae', 2, 1, 0, 'https://res.cloudinary.com/dplcs6tbo/image/upload/v1772713068/eunchae_xjw6ta.png')
 ON CONFLICT (idol_id) DO NOTHING;
