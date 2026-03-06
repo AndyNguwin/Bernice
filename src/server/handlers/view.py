cfe3e9d99a6194d585e40720b716da2fa7fef5e2
Andy Nguyen <iam.andynguwin@gmail.com>
2026-03-06 03:16:42 -0800

    Detailed view of card for view command

--- src/server/handlers/view.py
+++ src/server/handlers/view.py
@@ -1,21 +1,27 @@
 from src.infra.db.postgres_repository import PostgresRepository
 from src.app.models.inventoryCard import InventoryCard
+from datetime import datetime
 
 async def view_handler(user_id:int , repository: PostgresRepository, public_code:str):
     card: InventoryCard = await repository.get_card_by_public_code(public_code)
+    description = f"""
+        `Code:` {card.public_code}
+        `Idol:` {card.idol_name}
+        `Group/Solo:` {card.artist_name}
+        `Set Name:` {card.card_set}
+        `Print #:` {card.print_number}
+        `Owner:` <@{card.owner_id}>
+        `Obtained:` {card.acquired_date.strftime("%b %d, %Y • %I:%M %p")}
+    """
 
     return {
         "type": 4,
         "data": {
             "embeds": [
                 {
-                    "title": f"{card.idol_name} — {card.artist_name} [{card.card_set}]",
-                    "description": f"Rolled by <@{card.owner_id}>",
+                    "description": description,
                     "image": {
                         "url": card.image_url
-                    },
-                    "footer": {
-                        "text": f"Code {public_code} • Print {card.print_number}"
                     }
                 }
             ]
